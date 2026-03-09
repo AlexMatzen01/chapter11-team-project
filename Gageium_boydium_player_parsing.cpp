42698cd982807a3245b129f2b91c2919f1038f8c
gageb438 <gageb534@gmail.com>
2026-03-09 16:54:46 -0500

    All done

--- Gageium_boydium_player_parsing.cpp
+++ Gageium_boydium_player_parsing.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 
 using namespace std;
 
+// i didnt test this btw, sorry if it doesnt work.
+// its written to the info i have
+// its all commented
+
 int main()
 {
+	// Init variables
 	ofstream Output_File("File_Name.txt");
-	vector<Player>;
-
 	int Players = 0;
 	
-	if (Players <= 0)
+	// Get the players in the tournament
+	while (Players <= 0)
 	{
 		cout << "How many players are there in the tournament? :> ";
 		cin >> Players;
 	}
 
+	// Get the player info, write it to a file
 	for (int I = 0; I <= Players; I++)
 	{
+		// Create the player object and get all info.
 		Player My_Player;
 		cout << "-- Player " << I + 1 << "--" << endl;
 		cout << "Enter player name :> ";
@@ -32,8 +37,8 @@ int main()
 
 		cout << endl << endl;
 
-		Output_File << Player.name
+		// Write it to the file
+		Output_File << Player.name << "|" << Player.role << "|" << Player.kills << "|" << Player.deaths << endl;
+		// Format: Name|Role|Kills|Deaths
 	}
 }
-
-// Format: Name|Role|Kills|Deaths
